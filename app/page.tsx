d147235f47c4613216b0a8f94ea6bf99b277a93c
ferda-zeynep <ferdazeynepcapa@gmail.com>
2026-05-22 15:54:51 +0200

    Fix: Remove deprecated afterSignOutUrl from UserButton

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
                   Dashboard
                 </Button>
               </Link>
-              <UserButton afterSignOutUrl="/" />
+              <UserButton />
             </div>
           )}
         </div>
